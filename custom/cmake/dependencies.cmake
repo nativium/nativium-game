758d2d1ec3d183f622086bf27a206d6ac9079a6a
Paulo Coutinho <paulocoutinhox@gmail.com>
2022-09-24 20:08:27 -0300

    new architecture

--- custom/cmake/dependencies.cmake
+++ custom/cmake/dependencies.cmake
@@ -1,10 +1,12 @@
 # dependencies for tests
 include(${NATIVIUM_ROOT_PATH}/cmake/dependencies-tests.cmake)
 
-# sdl2
-find_package(SDL2 CONFIG REQUIRED)
-target_link_libraries(${NATIVIUM_PROJECT_NAME} PRIVATE SDL2::SDL2-static)
+if(NOT NATIVIUM_TARGET STREQUAL "wasm")
+    # sdl2
+    find_package(SDL2 CONFIG REQUIRED)
+    target_link_libraries(${NATIVIUM_PROJECT_NAME} PRIVATE SDL2::SDL2-static)
 
-# sdl2 image
-find_package(SDL2_image CONFIG REQUIRED)
-target_link_libraries(${NATIVIUM_PROJECT_NAME} PRIVATE SDL2_image::SDL2_image-static)
+    # sdl2 image
+    find_package(SDL2_image CONFIG REQUIRED)
+    target_link_libraries(${NATIVIUM_PROJECT_NAME} PRIVATE SDL2_image::SDL2_image-static)
+endif()
